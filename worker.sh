d8ab7f65b544737b2b766e0009ab33b40648b48c
GamePlayer-8 <gameplayer2019pl@tutamail.com>
2023-11-23 21:22:02 +0100

    Create website.

--- worker.sh
+++ worker.sh
@@ -5,5 +5,10 @@ SCRIPT_PATH="$(dirname "$(realpath "$0")")"
 cd "$SCRIPT_PATH"
 
 echo 'Executing setup...'
+sh scripts/set.sh res/parser.conf legacy.html
 sh scripts/set.sh res/parser.conf index.html
 sh scripts/set.sh res/parser.conf README.md
+
+sh index.sh
+
+sed -i 's/index.html//g' .gitignore
